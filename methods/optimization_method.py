9ac1cf5f3583097229c3adc897a5f1d82a171973
Peter Meisrimel <peter.meisrimel@gmail.com>
2020-09-21 20:01:49 +0200

    New branch for examples and testing

--- methods/optimization_method.py
+++ methods/optimization_method.py
@@ -1,5 +1,5 @@
-from methods.utils.gradient_hessian import *
 from scipy.optimize import fmin
+import numpy as np
 
 
 class OptimizationMethod:
@@ -21,16 +21,15 @@ class OptimizationMethod:
         Number of iterations that was used to solve the problem
     """
 
-    def __init__(self, x_0):
-        self.x_0 = x_0
-        self.x_k = x_0
-        self.s_k = None
-        self.nr_iteration = 0
+    def __init__(self):
+        # possibly track things, e.g., hessian evaluations
+        pass
 
+    # obsolete?
     def optimize(self, problem):
         raise NotImplementedError
 
-    def __find_alpha_exact_line_search(self, f):
+    def __find_alpha_exact_line_search(self, f, direction):
         """
         Perform exact line search to determine alpha_k
         that minimizes f(x_k + alpha_k * s_k)
@@ -39,9 +38,9 @@ class OptimizationMethod:
         """
 
         def h(alpha):
-            return f(self.x_k + alpha * self.s_k)
+            return f(self.x_k + alpha * direction)
 
-        alpha_0 = 0
+        alpha_0 = np.array([0])
 
         # disp = False prevents printing optimization result to console
         return fmin(h, alpha_0, disp=False)
@@ -52,31 +51,42 @@ class OptimizationMethod:
     def __find_acceptable_point(self):
         raise NotImplementedError
 
-    def newton_optimization(self, problem, use_exact_line_search=True, display_log=True):
+    def newton_optimization(self, problem, x0, use_exact_line_search=True, display_log=True,
+                            tol = 1e-8, maxiter = 100, callback = None):
         """
         Solve optimization using base Newton method (see 3.3)
         :param
             problem: OptimizationProblem
+        :param 
+            x0: Initial guess
+        :param 
+            tol: Tolerance for termination criterion
         :return:
         """
+        # Initialize iteration
+        self.x_k = np.copy(x0)
+        
         # x(*)
-        self.nr_iteration = 1
-        while True:
+        for nr_iterations in range(1, maxiter + 1):
             # Update s_k
-            self.__update_newton_direction(problem.function)
+            s_k = self.__get_newton_direction(problem)
             alpha = 1
             if use_exact_line_search:
-                alpha = self.__find_alpha_exact_line_search(problem.function)
-            x_k_plus_1 = self.x_k + alpha * self.s_k
+                alpha = self.__find_alpha_exact_line_search(problem.f, s_k)
+            x_k_plus_1 = self.x_k + alpha * s_k
 
-            # Break condition
-            if np.array_equal(self.x_k, x_k_plus_1):
+            # Break condition, update smaller than a prescribed tolerance
+            if np.linalg.norm(alpha * s_k, 2) < tol:
                 break
 
             # Reassign x_k
             self.x_k = x_k_plus_1
-            # Increment iteration count
-            self.nr_iteration += 1
+            # callback function, for e.g. tracking the iteration
+            if callback:
+                callback(self.x_k)
+            # update hessian
+            self.__update_hessian()
+            
 
         if display_log:
             successful_message = "Optimization successful using basic newton method"
@@ -84,27 +94,29 @@ class OptimizationMethod:
             line_search_message = "With use of exact line search" if use_exact_line_search else None
             if line_search_message is not None:
                 print(line_search_message)
-            number_of_iteration_message = f"Number of iteration: {self.nr_iteration}"
+            number_of_iteration_message = f"Number of iteration: {nr_iterations}"
             print(number_of_iteration_message)
-            optimization_result_message = f"Optimization result: {self.x_k}"
+            optimization_result_message = f"Optimum at : {self.x_k} with f(x_opt) = {problem.f(self.x_k)}"
             print(optimization_result_message)
             print()
 
-        return self.x_k
+        return self.x_k, nr_iterations
 
-    def __update_newton_direction(self, f):
+    def __get_newton_direction(self, problem):
         """
-        Update the newton direction vector
+        Get the newton direction vector
         :param
             f:
                 Objective function that needs to be optimized
         :return:
+            New newton direction
         """
-        gradient_matrix = grad(f)(self.x_k)
-        hessian_matrix = hessian(f)(self.x_k)
+        gradient_vector = problem.grad(self.x_k)
+        hessian_matrix = problem.hessian(self.x_k)
         # Newton direction (see 3.3)
-        self.s_k = -np.linalg.solve(hessian_matrix, gradient_matrix)
+        return np.linalg.solve(hessian_matrix, -gradient_vector)
 
+    # TODO: possibly needs some input parameters
     def __update_hessian(self):
         """
         Update H(k+1)
@@ -114,4 +126,4 @@ class OptimizationMethod:
         - BFGS
         :return:
         """
-        raise NotImplementedError
+        pass
--- optimization_problem.py
+++ optimization_problem.py
@@ -1,3 +1,5 @@
+import utils.gradient_hessian as utils_grad_hess
+
 class OptimizationProblem:
     """
         # Optimization problem class
@@ -5,18 +7,16 @@ class OptimizationProblem:
 
         Let f: Rn -> R the objective function
         Find x* such that f(x*) = {x € Rn | min f(x)}
-
-
-        Attributes
-        ----
-        function: Function
-            objective function
     """
 
-    def __init__(self, function):
-        self.function = function
-        # Check is the function is objective and raise error
-
-
-    def __check_objective(self, function):
-        raise NotImplementedError
+    def __init__(self):
+        pass
+        
+    def f(self, x):
+        return None
+    
+    def grad(self, x):
+        return utils_grad_hess.grad(self.f)(x)
+    
+    def hessian(self, x):
+        return utils_grad_hess.hessian(self.f)(x)
